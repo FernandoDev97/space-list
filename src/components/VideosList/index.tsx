8af880127ffddc5ecf14be972130cec89b82cc63
Fernando Souza <l.fernando.dev@gmail.com>
2023-04-23 23:25:20 -0300

    add context list video

--- src/components/VideosList/index.tsx
+++ src/components/VideosList/index.tsx
@@ -1,24 +1,22 @@
+import { useList } from "../../context/ListContent";
+import { useLoading } from "../../context/LoadingContexts";
 import { Title } from "../Title";
 import { VideoItem } from "../VideoItem";
 import { Container, ListContainer } from "./styles";
 
 export function VideosList() {
+  const { list } = useList()
+  const { isLoading } = useLoading()
+
   return (
     <Container>
       <header>
         <Title>Videos in list</Title>
       </header>
       <ListContainer>
-        <VideoItem />
-        <VideoItem />
-        <VideoItem />
-        <VideoItem />
-        <VideoItem />
-        <VideoItem />
-        <VideoItem />
-        <VideoItem />
-        <VideoItem />
-        <VideoItem />
+        {!isLoading && list.map(video => (
+          <VideoItem key={video.id} data={video}/>
+        ))}
       </ListContainer>
     </Container>
   );
